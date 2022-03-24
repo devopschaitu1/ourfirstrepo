b4deb4f421b547e0fbef6300b4c00d618c24eb62
devopschaitu1 <knowledgeit888@gmail.com>
2022-03-24 10:16:57 +0530

    modified files

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
-}
-
 resource "aws_instance" "instanceformterraform" {
   ami           = "ami-0e1d30f2c40c4c701"
   instance_type = "t2.micro"
